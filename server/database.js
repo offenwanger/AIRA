6203ca89683997fe184119877ef3c8571cceddf4
Anna Offenwanger <pixiegames.inc@gmail.com>
2019-03-12 11:51:28 -0700

    Split text into sentences

--- server/database.js
+++ server/database.js
@@ -6,38 +6,12 @@ let db;
 exports.storePDF = function(filename) {
   let db = getDB();
 
-  let insertQuery = `
-    INSERT INTO Pdfs (filename)
-    VALUES (` + filename + `);
-    `;
-
-  db.run(insertQuery);
-
-  const sqlite3 = require('sqlite3').verbose();
-  
-  let sql = `SELECT * FROM Pdfs WHERE filename LIKE '%` + filename + `%'`;
-  
-  db.all(sql, [], (err, rows) => {
-    console.log("got something");
-    if (err) {
-      throw err;
-    }
-
-    rows.forEach((row) => {
-      console.log(row.filename)
-      console.log(row.id);
-    });
-  });
-  
-  // close the database connection
-  db.close();
-
-  fileToSentences(filename).then((pdfData) => {
-    console.log("Got the data");
-  }, (error) => {
-    console.log("PDF parsing error");
-    console.error(error);
-  });
+  Promise.all([insertPDF(db, filename), fileToSentences(filename)]).then((results) => {
+    let pdfID = results[0];
+    let fileValues = results[1];
+    console.log("all promises resolved");
+    console.log(fileValues);
+  })
 }
 
 function fileToSentences(filename) {
@@ -45,36 +19,47 @@ function fileToSentences(filename) {
     let pdfParser = new PDFParser();
 
     pdfParser.on("pdfParser_dataError", errData => {
-      reject(errData.parserError);
+      reject("Error while extracting PDF text: " + errData.parserError);
     });
 
-    let db = getDB();
     let pageNumber = 1;
     let lineText = "";
-    let PDF
+    let extractedData = [];
   
     pdfParser.on("pdfParser_dataReady", pdfData => {
-      let lines = [];
+      let data = {startPage:pageNumber};
       pdfData.formImage.Pages.forEach((page)=>{
         page.Texts.forEach((text)=>{
           text.R.forEach((textRun)=>{
-            lines.push(decodeURIComponent(textRun.T));
+            lineText += textRun.T;
+            // TODO: fix the issue with this breaking up decimal numbers etc.
+            var sentenceTest = lineText.match(/([^\.!\?]+[\.!\?]+)|([^\.!\?]+$)/g);
+            // Check if the line splits into sentences. 
+            // There may be multiple sentences in a single line
+            while(sentenceTest.length > 1) {
+              data.endPage = pageNumber;
+              data.lineText = decodeURIComponent(sentenceTest.shift());
+              extractedData.push(data);
+              // This is logically sound because only the first complete 
+              // sentence could have been on the previous page
+              data = {startPage:pageNumber};
+            }
+            lineText = sentenceTest.shift();
           });
-        })
-      });
-  
-      console.log(lines.join(" "));
-  
-      resolve(lines);
-      
-      fs.writeFile(__dirname + '/PDFs/test.json', JSON.stringify(pdfData), 
-        function(err, result) {
-          if(err) {
-            console.log('error', err);
-          } else {
-            console.log("JSON written");
-          }
         });
+        pageNumber++;
+      });
+
+      resolve(extractedData);
+      
+      // fs.writeFile(__dirname + '/PDFs/test.json', JSON.stringify(pdfData), 
+      //   function(err, result) {
+      //     if(err) {
+      //       console.log('error', err);
+      //     } else {
+      //       console.log("JSON written");
+      //     }
+      //   });
     });
   
     pdfParser.loadPDF(__dirname + '/PDFs/' + filename);
@@ -92,12 +77,15 @@ function getDB() {
     }
     console.log('Database connection established.');
 
-    let tablesetup = `
+    let sql = `
       CREATE TABLE IF NOT EXISTS Pdfs (
         id INTEGER PRIMARY KEY, 
         filename TEXT
       );
+    `;
+    db.run(sql);
 
+    sql = `
       CREATE TABLE IF NOT EXISTS Sentences (
         id INTEGER PRIMARY KEY, 
         pdf INTEGER,
@@ -107,10 +95,32 @@ function getDB() {
         line_text TEXT,
         FOREIGN KEY(pdf) REFERENCES Pdfs(id)
       );
-    `
-    db.run(tablesetup);
-
+    `;
+    db.run(sql);
   });
 
   return db;
+}
+
+function insertPDF(db, filename) {
+  return new Promise((resolve, reject) => {
+    // TODO(offenwanger): If there is a duplicate filename or duplicate upload,
+    // then this will duplicate the entry
+    let sql = `INSERT INTO 'Pdfs' (filename) VALUES ('` + filename + `');`;
+    db.run(sql);
+    sql = `SELECT * FROM 'Pdfs' WHERE filename LIKE '%` + filename + `%'`;
+    db.get(sql, [], (err, row) => {
+      if (err) {
+        reject(error);
+        return;
+      }
+
+      if(!row || !row.id) {
+        reject("Error while retriving PDF id for "+filename+"!");
+        return;
+      }
+
+      resolve(row.id);
+    });
+  });
 }
